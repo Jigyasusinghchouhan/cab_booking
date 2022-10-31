a1832220c224818b80d1d58b04a8eebfc3f317f7
Gaurav_Gaba <gauravvg2000@gmail.com>
2022-10-31 10:04:43 +0530

    Gaurav's second commit(Made stateless widget for hot reload feature and created Scaffold with circle Avatar. next I am drawing a logo in paint for our app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp( const MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home:Scaffold(
+    backgroundColor: Colors.tealAccent.shade200,
+      body: SafeArea(child:
+      Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+         CircleAvatar(
+
+         ),
+        ],
+      ),
+      ),
+    ),
+    );
+  }
+}
 
