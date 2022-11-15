d794087056858130cd1d5b2e7dcb5737f01fdcc7
Meeta_Nahar <naharmeeta@gmail.com>
2022-11-15 10:36:33 +0530

    navigation

--- cab_booking_user/lib/main.dart
+++ cab_booking_user/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cab_booking_user/Authenticate.dart';
 import 'LoginScreen.dart';
+import 'package:get/get.dart';
+
+
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -14,7 +17,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    print("ds");
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
