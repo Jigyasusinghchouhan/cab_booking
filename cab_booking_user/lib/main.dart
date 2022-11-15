2db8203c9f5c014170255703798925cfb22623e7
JIGYASU_SINGH <jigyashu2001@gmail.com>
2022-11-15 08:20:00 +0530

    update

--- cab_booking_user/lib/main.dart
+++ cab_booking_user/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
