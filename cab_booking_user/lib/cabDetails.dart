9249354604272d805f65191bebf736e5f633f843
Neha More <moreneha169@gmail.com>
2022-11-22 09:42:36 +0530

    booking page added

--- cab_booking_user/lib/cabDetails.dart
+++ cab_booking_user/lib/cabDetails.dart
@@ -8,9 +8,7 @@ final String carType;
   Widget build(BuildContext context) {
     var appHeadingTitle;
 
-    return MaterialApp(
-
-      home: Scaffold(
+    return Scaffold(
           appBar: AppBar(
             title: Text('Mini'),
             centerTitle: true,
@@ -22,59 +20,49 @@ final String carType;
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  myData(Data: 'Booking Details'),
-
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: Get.width / 6, right: Get.width / 6),
-                    child: Divider(
-                      thickness: 2,
+                  Expanded(
+                    flex: 2,
+                    child: myData(Data: 'Booking Details'),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: Get.width / 6, right: Get.width / 6),
+                      child: Divider(
+                        thickness: 2,
+                      ),
                     ),
                   ),
-                  details(deta: 'Ride Type',detb:'Dropping' ),
-                  Container(
-                    height: 40.0,
-                    color: Colors.white,
-                    child: Text('Car Type -'),
+                  Expanded(
+                    flex: 10,
+                    child: Column(
+                      children: [
+                        details(deta: 'Ride Type', detb: 'Dropping'),
+                        details(deta: 'Car Type', detb: 'Dropping'),
+                        details(deta: 'From', detb: 'Dropping'),
+                        details(deta: 'To', detb: 'Dropping'),
+                        details(deta: 'Distance', detb: 'Dropping'),
+                        details(deta: 'Date/Time', detb: 'Dropping'),
+                        details(deta: 'Total fare', detb: 'Dropping'),
+                      ],
+                    ),
                   ),
 
-                  Container(
-                    height: 40.0,
-                    color: Colors.white,
-                    child: Text('From -'),
+                  Expanded(
+                    flex: 2,
+                    child: myData(
+                      Data: "Passenger Details",
+                    ),
                   ),
-
-                  Container(
-                    height: 40.0,
-                    color: Colors.white,
-                    child: Text('To -'),
-                  ),
-
-                  Container(
-                    height: 40.0,
-                    color: Colors.white,
-                    child: Text('Distance -'),
-                  ),
-
-                  Container(
-                    height: 40.0,
-                    color: Colors.white,
-                    child: Text('Date/Time -'),
-                  ),
-
-                  Container(
-                    height: 40.0,
-                    color: Colors.white,
-                    child: Text('Total fare -'),
-                  ),
-                  myData(
-                    Data: "Passenger Details",
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: Get.width / 6, right: Get.width / 6),
-                    child: Divider(
-                      thickness: 2,
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: Get.width / 6, right: Get.width / 6),
+                      child: Divider(
+                        thickness: 2,
+                      ),
                     ),
                   ),
 
@@ -88,135 +76,136 @@ final String carType;
                               ),
                               ),
                     ),*/
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      hintText: 'What do people call you?',
-                      labelText: 'Name *',
+                  Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        hintText: 'What do people call you?',
+                        labelText: 'Name *',
+                      ),
+                      onSaved: (String? value) {
+                        // This optional block of code can be used to run
+                        // code when the user saves the form.
+                      },
+                      validator: (String? value) {
+                        return (value != null && value.contains('@'))
+                            ? 'Do not use the @ char.'
+                            : null;
+                      },
                     ),
-                    onSaved: (String? value) {
-                      // This optional block of code can be used to run
-                      // code when the user saves the form.
-                    },
-                    validator: (String? value) {
-                      return (value != null && value.contains('@'))
-                          ? 'Do not use the @ char.'
-                          : null;
-                    },
                   ),
 
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      hintText: 'optional?',
-                      labelText: 'Email *',
+                  Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        hintText: 'optional?',
+                        labelText: 'Email *',
+                      ),
+                      onSaved: (String? value) {
+                        // This optional block of code can be used to run
+                        // code when the user saves the form.
+                      },
+                      validator: (String? value) {
+                        return (value != null && value.contains('@'))
+                            ? 'Do not use the @ char.'
+                            : null;
+                      },
                     ),
-                    onSaved: (String? value) {
-                      // This optional block of code can be used to run
-                      // code when the user saves the form.
-                    },
-                    validator: (String? value) {
-                      return (value != null && value.contains('@'))
-                          ? 'Do not use the @ char.'
-                          : null;
-                    },
                   ),
 
-                  Container(
+                  Expanded(
+                    flex: 2,
+
                     //height: 60.0,
                     //color: Colors.white,
                     // child: Text('Button '),
-                    child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              height: 30,
-                              width: 100,
-                              decoration: ShapeDecoration(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                  ),
-                                  color: Colors.cyan),
-                              // elevation :30.0,
-                              // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(60.0)),
 
-                              child: Center(child: Text('Button')),
-                            ),
-                          ),
-                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top:18.0),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Container(
+
+                          decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                              color: Colors.black),
+                          // elevation :30.0,
+                          // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(60.0)),
+
+                          child: Center(child: Text('Button',style: TextStyle(color: Colors.white),)),
+                        ),
+                      ),
+                    ),
+
                   ),
+
 
                   // ],
                 ],
               ),
             ),
-          )),
-    );
+          ));
 
 
   }
 
-  details({String? deta, String? detb}) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            height: 40.0,
-            color: Colors.white,
-            child: const Text('Ride Type'),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Text("-"),
-        ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            height: 40.0,
-            color: Colors.white,
-            child: const Text('Dropping'),
-          ),
-        ),
-      ],
-    );
-  }
-  myData({String ?Data, int ?vals}){
-    print(vals.toString());
+  myData({String? Data}) {
     return Column(
       children: [
         Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text("${Data!}",
-              style:
-              TextStyle(
-                  color:new Color(0xff622F74),
+            child: Text(
+              "${Data!}",
+              style: TextStyle(
+                  color: new Color(0xff622F74),
                   fontWeight: FontWeight.bold,
-                  fontSize: 30.0
-              ),
-
+                  fontSize: 30.0),
             ),
           ),
         ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(vals.toString()!="null"?vals.toString():"",
-              style:
-              TextStyle(
-                  color:new Color(0xff622F74),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0
-              ),
+      ],
+    );
+  }
 
+  details({String? deta, String? detb}) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 3,
+          child: Container(
+            height: 40.0,
+            color: Colors.white,
+            child: Text(
+              deta!,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
           ),
         ),
-
+        Expanded(
+          flex: 1,
+          child: Text(
+            "-",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+        Expanded(
+          flex: 8,
+          child: Container(
+            height: 40.0,
+            color: Colors.white,
+            child: Text(
+              detb!,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ),
+        ),
       ],
     );
   }
